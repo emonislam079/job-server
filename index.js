c85d444d73f7b968c7f59a15538b9addf295345e
emonislam079 <emonislam079@gmail.com>
2022-01-27 00:30:29 +0600

    admin

--- index.js
+++ index.js
@@ -93,6 +93,19 @@ async function run() {
             res.json(result);
             
         })
+
+        // set admin email
+        
+        app.get('/users/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if(user?.role === 'admin'){
+                isAdmin = true;
+            }
+            res.json({admin: isAdmin})
+        })
        
     }
     finally{
